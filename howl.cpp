e6ec80b3faa246e5e1f741d67743d861c75bdd9d
connorsimms <connorvsimms@gmail.com>
2025-12-04 15:35:00 -0600

    https://open.kattis.com/problems/howl

---
+++ howl.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    string fen; cin >> fen;
+
+    cout << "AWAWHOO";
+    string word = "WOWO";
+    for(long long i = 0 ; i <= (long long)fen.size() / 4; ++i) 
+    {
+	cout << word;
+    }
+    cout << '\n';
+}
