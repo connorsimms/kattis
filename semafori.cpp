f696681e66aa69dca6ae81b00b70bd413875e326
connorsimms <connorvsimms@gmail.com>
2025-12-09 11:33:44 -0600

    semafori
    https://open.kattis.com/problems/semafori

---
+++ semafori.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N, L; cin >> N >> L;
+    int t = 0;
+    int l = 0;
+    for (int i = 0 ; i < N ; ++i)
+    {
+        int D, R, G; cin >> D >> R >> G;
+        
+        t += (D - l);
+        l = D;
+
+        if (t % (R + G) <= R) { t += R - (t % (R + G)); }
+    }
+
+    t += (L - l);
+
+    cout << t << '\n';
+}
